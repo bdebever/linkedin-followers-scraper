88d35ee8580c774bb12e47f92fc144d557da6d8f
Baptiste Debever <baptiste.debever@gmail.com>
2020-08-03 14:12:34 +0200

    Added prettier

--- fetcher.js
+++ fetcher.js
@@ -1,99 +1,97 @@
-'use strict';
+"use strict";
 
-require('dotenv').config();
-const request = require('request-promise-native');
-const fs = require('fs');
-const fastcsv = require('fast-csv');
-const ws = fs.createWriteStream('followers.csv');
-const ROOT_URL = 'https://www.linkedin.com/voyager/api/voyagerOrganizationDashFollowers?decorationId=com.linkedin.voyager.dash.deco.organization.FullFollowers-1&q=organization';
+require("dotenv").config();
+const request = require("request-promise-native");
+const fs = require("fs");
+const fastcsv = require("fast-csv");
+const ws = fs.createWriteStream("followers.csv");
+const ROOT_URL =
+  "https://www.linkedin.com/voyager/api/voyagerOrganizationDashFollowers?decorationId=com.linkedin.voyager.dash.deco.organization.FullFollowers-1&q=organization";
 
 const BATCH = 100;
 
 // Complete those variables into an .env file
 const { TOTAL_PAGE_FOLLOWERS, ORGA_ID, COOKIES, CSRF_TOKEN } = process.env;
 
-const sleep = (s) => new Promise(resolve => setTimeout(resolve, s*1000));
+const sleep = (s) => new Promise((resolve) => setTimeout(resolve, s * 1000));
 
 const main = async (organizationId, total) => {
+  let results = [];
+  let moreFollowers = true;
+  let index = 0;
 
-    let results = [];
-    let moreFollowers = true;
-    let index = 0;
+  while (moreFollowers) {
+    console.log(index);
+    let currentIndex = index * BATCH;
 
-    while (moreFollowers) {
+    let {
+      data: { paging },
+      included,
+    } = await callApi(organizationId, currentIndex);
 
-        console.log(index);
-        let currentIndex = index * BATCH;
+    console.log(`Got ${included.length} profiles`);
 
-        let { data: { paging }, included } = await callApi(organizationId, currentIndex)
+    // Improve data part
+    results = [
+      ...results,
+      ...included
+        .filter((item) => item.firstName)
+        .map((item) => ({
+          profileUrl: formatProfileUrl(item.entityUrn),
+          firstName: item.firstName || "",
+          lastName: item.lastName || "",
+          headline: item.headline || "",
+        })),
+    ];
 
-        console.log(`Got ${included.length} profiles`)
+    console.log(`We currently have ${results.length} profiles collected`);
 
-        // Improve data part
-        results = [...results, ...included.filter(item => item.firstName).map(item => ({
-        	profileUrl: formatProfileUrl(item.entityUrn),
-        	firstName: item.firstName || '',
-        	lastName: item.lastName || '',
-        	headline: item.headline || ''
-        }))];
+    moreFollowers = paging && paging.total >= currentIndex;
 
-        console.log(`We currently have ${results.length} profiles collected`)
+    // Random number between 2 and 8
+    await sleep(Math.floor(Math.random() * 7) + 2);
+    index++;
+  }
 
-        moreFollowers = (paging && paging.total >= currentIndex)
+  console.log(`Finished collecting ${results.length} followers`);
 
-        // Random number between 2 and 8
-        await sleep((Math.floor(Math.random() * 7)) + 2);
-        index++;
-
-    }
-
-    console.log(`Finished collecting ${results.length} followers`)
-
-    return results;
-
+  return results;
 };
 
-const formatProfileUrl = (url) => url.replace('urn:li:fsd_profile:', 'https://linkedin.com/in/');
+const formatProfileUrl = (url) =>
+  url.replace("urn:li:fsd_profile:", "https://linkedin.com/in/");
 
 const callApi = async (organizationId, start) => {
+  console.log(start);
 
-	console.log(start);
+  const options = {
+    method: "GET",
+    uri: `${ROOT_URL}&organization=urn%3Ali%3Afsd_company%3A${organizationId}&start=${start}&count=${BATCH}`,
+    headers: {
+      authority: "www.linkedin.com",
+      accept: "application/vnd.linkedin.normalized+json+2.1",
+      "csrf-token": CSRF_TOKEN,
+      "sec-fetch-site": "same-origin",
+      cookie: COOKIES,
+    },
+    json: true,
+  };
 
-    const options = {
-        method: 'GET',
-        uri: `${ROOT_URL}&organization=urn%3Ali%3Afsd_company%3A${organizationId}&start=${start}&count=${BATCH}`,
-        headers: {
-            'authority': 'www.linkedin.com',
-            'accept': 'application/vnd.linkedin.normalized+json+2.1',
-            'csrf-token': CSRF_TOKEN,
-            'sec-fetch-site': 'same-origin',
-            'cookie': COOKIES
+  let results = {};
+  try {
+    results = await request(options);
+    //console.log(results)
+  } catch (error) {
+    console.log(error);
+  }
 
-        },
-        json: true
-    };
-
-
-    let results={};
-    try {
-        results = await request(options);
-        //console.log(results)
-    } catch (error) {
-        console.log(error);
-    }
-
-    return results;
-}
+  return results;
+};
 
 const saveToCsv = (data) => {
-	fastcsv
-	  .write(data, { headers: true })
-	  .pipe(ws);
-}
-
+  fastcsv.write(data, { headers: true }).pipe(ws);
+};
 
 main(ORGA_ID, TOTAL_PAGE_FOLLOWERS)
-    .then(value => {
-        const data = saveToCsv(value);
-    })
-    .catch(e => console.log(`error: ${e}`))
+  .then((value) => saveToCsv(value))
+  .catch((e) => console.log(`error: ${e}`));
